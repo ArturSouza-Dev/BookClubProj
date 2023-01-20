c756b8ac0a3a047fc31e3cdbe809c3e37fb0bff8
Artur Souza <artur95gss@gmail.com>
2023-01-19 20:04:31 -0500

    homePageHTML improved

--- WebPages/home.php
+++ WebPages/home.php
@@ -3,6 +3,7 @@
     include_once('../DataStructure/context.php');
     include_once('../Application/commonFunctions.php');
 	include_once('partials/header.php');
+    
 
     Context::loadGroups();
     Context::loadBooks();
@@ -41,52 +42,24 @@
 <div class="pageBlock">                                                                                              
     <?php    
         foreach(Context::$groups as $myGroups){
-            if(($myGroups->groupCreator == $userName || $myGroups->groupAdm1 == $userName || $myGroups->groupAdm2 == $userName) || in_array($userName, $myGroups->participants)){
+            if(($myGroups->groupCreator == $userName || $myGroups->groupAdm1 == $userName || $myGroups->groupAdm2 == $userName) || in_array($userName, $myGroups->participants)){                
                 ?>
-                <form class="homePageBlock" method="GET"> 
+                <form class="homePageBlock" method="get"> 
                     <ul>                           
-                        <li ><h1><?php echo $myGroups->groupName ?></h1></li>
+                        <li ><h1><a href="group.php?selectedGroup=<?php echo $myGroups->groupName ?>"><?php echo $myGroups->groupName ?></a></h1></li>
                         <?php
-                        foreach(Context::$books as $currentBook){
-                            if($currentBook->bookId == $myGroups->bookId){
-                                ?><li><h2><?php echo "Currently reading: ".$currentBook->bookName ?></h2></li><?php
+                            foreach(Context::$books as $currentBook){
+                                if($currentBook->bookId == $myGroups->bookId){
+                                    ?><li><h2><?php echo "Currently reading: ".$currentBook->bookName ?></h2></li><?php
+                                }
                             }
-                        }
-                        ?>                            
-                        <!--<input type="submit" name="redirectToGroupPage" value="Access">-->
-                        </a>
+                        ?>                                                    
                         <li><?php echo "=======================================================" ?></li>     
                     </ul>
                 </form>                                                                                
                 <?php    
             }
-        }
-    /*
-        $sql = "SELECT groupName, bookName FROM groups, books WHERE groups.currentBookId = books.bookId";
-        $statement = $pdo->prepare($sql);
-        $statement->execute();                 
-        foreach($statement as $row){
-            $sql = "SELECT groupName FROM groups WHERE groupCreator = '$userName' OR groupAdm1 = '$userName' OR groupAdm2 = '$userName' OR participant1 = '$userName' OR participant2 = '$userName' OR participant3 = '$userName' OR participant4 = '$userName' OR participant5 = '$userName'";
-            $stmt = $pdo->prepare($sql);
-            $stmt->execute(); 
-            foreach($stmt as $row2) {
-                if ($row["groupName"] == $row2["groupName"]) {
-                    ?>
-                    <form method="GET"> 
-                        <ul>
-                        <a href="group.php?groupName=<?php echo $row["groupName"] ?> ">
-                            <li ><h1><?php echo "Group: " .$row["groupName"] ?></h1></li>
-                            <li><h2><?php echo "Currently reading: ".$row["bookName"] ?></h2></li>
-                            <!--<input type="submit" name="redirectToGroupPage" value="Access">-->
-                            </a>
-                            <li><?php echo "=======================================================" ?></li>     
-                        </ul>
-                    </form>                                                                                
-                    <?php                            
-                }
-            }
-        } 
-        */                    
+        }         
     ?>        
 </div>                                     
                 
